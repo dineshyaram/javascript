45c3784e29a9dd0db70f7998c6e1b2c57554eb64
dineshyaram <dineshyaram.eidiko@gmail.com>
2025-04-08 10:51:36 +0000

    datatypes

---
+++ jsp/datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all javascript code as newer version 
+
+// alert(3+3)  // we are using node.js not windows console ( that why we get error if we want use here )
+ 
+// Datatypes:
+console.log(2e53);
+
+/* number --> 2 to power 53 
+   string --> string respresent  "" or '' in javascript
+   bigint  --> is used for bigger value 
+   boolean --> true or false
+   undefined --> type of is undefined
+   null --> type is object
+   symbol --> 
+
+*/
