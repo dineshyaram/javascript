babc557e13e4a4e7cdcbefbfe6eb29bc3e6e692e
dineshyaram <dineshyaram.eidiko@gmail.com>
2025-04-08 10:00:17 +0000

    variables

---
+++ jsp/variables.js
@@ -0,0 +1,8 @@
+const accountId = 1235556
+let accountName = "dinesh"
+var accountCity = "nizamabad"   // var was not in use that why we dont use it
+accountName = "shwet"
+
+accountNamess = "dineshssssss"
+console.log(accountNamess);
+
