b1fecd6b4db0cd039e681e347d95ade321e4b955
dineshyaram <dineshyaram.eidiko@gmail.com>
2025-04-08 11:22:56 +0000

    conversions of datatypes

---
+++ jsp/conversionofdatatypes.js
@@ -0,0 +1,16 @@
+let x = "22"
+console.log(typeof x);  //string
+
+let y = 22
+console.log(typeof y); //number 
+
+let z = null 
+console.log(typeof z); //object
+
+let isLoggedIn = 1
+let a = Boolean(isLoggedIn)
+console.log(a);   // output is true 
+
+let data = undefined
+console.log(typeof data); // undefined
+
