e8210c981991930ea21ddfeed65d782cd4c63a09
dineshyaram <dineshyaram.eidiko@gmail.com>
2025-04-09 11:44:59 +0000

    date

---
+++ jsp/datesandtimes.js
@@ -0,0 +1,6 @@
+let myDate = new Date()
+console.log(myDate); //(output)2025-04-09T11:29:06.520Z()
+
+console.log(myDate.toString());   // Wed Apr 09 2025 11:29:06 GMT+0000 (Coordinated Universal Time)
+console.log(myDate.toDateString()); // Wed Apr 09 2025
+console.log(myDate.toLocaleTimeString()); //  11:31:04 AM
