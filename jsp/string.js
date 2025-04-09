6882340dddbb6c09141c830e1edc87cadfcbae78
dineshyaram <dineshyaram.eidiko@gmail.com>
2025-04-09 10:59:07 +0000

    strings

---
+++ jsp/string.js
@@ -0,0 +1,17 @@
+// const name = "dinesh"
+// const id = 1244
+// console.log(`hello my name is ${name} and my id is ${id}`);
+// console.log(name.charAt(2));
+// const newName = new String("kanna")
+// console.log(newName);
+
+// console.log(newName.indexOf("a"));
+const id = "1234 5678 9325"   // this used to mask the data
+const name = "dinesh yaram "
+const newId = '*'.repeat(9) + id.slice(9,14) 
+console.log(newId);
+
+// const id = "1234 5678 9325"
+// const name = "dinesh yaram "
+// const newId = "**** ****"+ id.slice(9,14) 
+// console.log(newId);
